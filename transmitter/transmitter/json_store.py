b0ca7804722e8261beba0116989a51e461ba9365
= <=>
2025-06-29 07:41:21 -0400

    json_store update 0

---
+++ transmitter/transmitter/json_store.py
@@ -0,0 +1,45 @@
+import urllib
+import json
+import os
+
+from flask import current_app
+
+
+
+store_ids = [
+    "steam_top_sellers",
+    "mockarooo"
+]
+
+
+
+def json_store(args):
+    try:
+        instance_folder = current_app.instance_path
+    except:
+        # -600 : instance folder not found
+        return '-600'
+    
+    try:
+        json_id = args[0]
+    except:
+        # -601 : error getting request args
+        return '-601'
+    
+    if json_id in store_ids:
+        json_path = instance_folder + '/store/' + json_id + '.json'
+        if os.path.exists(json_path):
+            try:
+                with open(json_path, 'r') as f:
+                    to_return = json.load(f)
+                return to_return
+            except:
+                # -604 : file opening error
+                return '-604'
+        else:
+            # -603 : file not found
+            return '-603'
+    else:
+        # -602 : invalid store id
+        return '-602'
+
