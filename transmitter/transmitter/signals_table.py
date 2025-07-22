b1cb7de94e4bcbad89a7174ac6d82eb684db1b28
= <=>
2025-07-22 11:40:04 -0400

    cleanup:signals_table

--- transmitter/transmitter/signals_table.py
+++ transmitter/transmitter/signals_table.py
@@ -1,12 +1,6 @@
 valid_signals = [
-    # rss.py
-    'rss_headlines',
-    # google.py
-    'daily_trends',
-    # wikipedia.py
-    'random_page',
-    #steam_top_sellers.py
-    'get_steam_top_sellers',
     # json_store.py
-    'json_store'
+    'json_store',
+    # rapport_echo.py
+    'rapport_echo'
 ]
